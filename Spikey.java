b775ef7ad648b8dc638bf152baa4aec4ba9d28fb
imaginationmadi <145612004+imaginationmadi@users.noreply.github.com>
2023-09-29 04:03:50 -0700

    Spikey
    Program that prints out a spikey ball, or a spikey monster friend.
    
    This was a challenge in printing out the "\" properly.

---
+++ Spikey.java
@@ -0,0 +1,13 @@
+// spikey chapter one assignment 1
+
+public class Spikey{
+	
+	public static void main (String[] args)  {
+		System.out.println ("    \\/ ");
+		System.out.println ("   \\\\// ");
+		System.out.println ("  \\\\\\/// ");
+		System.out.println ("  ///\\\\\\ ");
+		System.out.println ("   //\\\\ ");
+		System.out.println ("    /\\ ");
+	}
+}
